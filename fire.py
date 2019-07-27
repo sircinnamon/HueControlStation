b6fdc216d898b60922a1862a6792bedd3cfc5278
Sir Cinnamon <sircinnamon@gmail.com>
2019-07-27 16:35:40 -0600

    Improve fire more, add ability to stop threads

--- fire.py
+++ fire.py
@@ -20,28 +20,28 @@ url = "http://10.0.0.34:8080/post/"
 group = Group(b, "Office")
 lights = group.lights
 media_controller = None
-
+stop_threads = False
 
 fire_graph = {
 	"base_red_1": {
 		"transition_to": 1,
-		"hue": 936,
+		"hue": 4000,
 		"saturation": 253,
 		"brightness": 141,
 		"edges": [
 			{
 				"dest": "base_red_2",
-				"weight": 0.66
+				"weight": 0.80
 			},
 			{
-				"dest": "flicker_orange",
-				"weight": 0.66
+				"dest": "flicker_orange_up",
+				"weight": 0.20
 			}
 		]
 	},
 	"base_red_2": {
-		"transition_to": 1,
-		"hue": 64988,
+		"transition_to": 3,
+		"hue": 4000,
 		"saturation": 95,
 		"brightness": 203,
 		"edges": [
@@ -52,10 +52,10 @@ fire_graph = {
 		]
 	},
 	"base_red_3": {
-		"transition_to": 1,
-		"hue": 321,
+		"transition_to": 3,
+		"hue": 4000,
 		"saturation": 247,
-		"brightness": 45,
+		"brightness": 100,
 		"edges": [
 			{
 				"dest": "base_red_1",
@@ -63,7 +63,7 @@ fire_graph = {
 			}
 		]
 	},
-	"flicker_orange": {
+	"flicker_orange_up": {
 		"transition_to": 0.2,
 		"hue": 5363,
 		"saturation": 254,
@@ -82,11 +82,23 @@ fire_graph = {
 		"brightness": 254,
 		"edges": [
 			{
+				"dest": "flicker_orange_down",
+				"weight": 1
+			}
+		]
+	},
+	"flicker_orange_down": {
+		"transition_to": 0.2,
+		"hue": 5363,
+		"saturation": 254,
+		"brightness": 200,
+		"edges": [
+			{
 				"dest": "base_red_1",
 				"weight": 1
 			}
 		]
-	}
+	},
 }
 
 def get_next_node(curr_id, graph):
@@ -104,6 +116,7 @@ def follow_graph_independent(graph, lights):
 	# TODO: Fuzz timings somehow to reduce sync'd transitions?
 	# TODO: signal thread to return to original_state and die?
     def light_thread(graph, light):
+        global stop_threads
         original_state = {
             "on": light.on,
             "brightness": light.brightness,
@@ -119,15 +132,26 @@ def follow_graph_independent(graph, lights):
                 "sat": graph[current_node]["saturation"] if "saturation" in graph[current_node] else light.saturation,
                 "bri": graph[current_node]["brightness"] if "brightness" in graph[current_node] else light.brightness
             }
-            print("{} set to {}".format(light.light_id, current_node))
-            print(command["transitiontime"])
+            # print("{} set to {}".format(light.light_id, current_node))
             b.set_light(light.light_id, command)
             time.sleep(command["transitiontime"]/10)
             current_node = get_next_node(current_node, graph)
+            if(stop_threads): break
+        command = {
+                "on":  original_state["on"],
+                "hue": original_state["hue"],
+                "sat": original_state["saturation"],
+                "bri": original_state["brightness"]
+        }
+        b.set_light(light.light_id, command)
 
+    threads = []
     for l in lights:
         t = threading.Thread(target=light_thread, args=(fire_graph, l))
         t.start()
+        fire_threads.append(t)
+    return threads
+
 
 
 def pushed_up(event):
@@ -136,6 +160,8 @@ def pushed_up(event):
 		l.on = (not l.on)
 
 def fire():
+	global stop_threads
+	stop_threads = False
 	follow_graph_independent(fire_graph, lights)
 
 def init_chromecast():
@@ -154,6 +180,11 @@ if __name__ == "__main__":
 	@skywriter.tap(position="center", repeat_rate=3)
 	def tap_center():
 		fire();
+
+	@skywriter.tap(position="south", repeat_rate=3)
+	def tap_south():
+		global stop_threads
+		stop_threads = (not stop_threads)
 
 	# media_controller = init_chromecast()
 	print("ready")
